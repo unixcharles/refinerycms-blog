525be3b001358f7615603b0e9ea0abcb77c57285
Philip Arndt <parndt@gmail.com>
2010-03-11 13:23:25 +1300

    Made the emails use actual mailer templates rather than strings in the model. Changed the way that the notification email looks, changed some wording and fixed grammatical issues.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,25 +1,23 @@
 class Blog < ActiveRecord::Base
   acts_as_taggable_on :categories, :tags, :authors
   has_many :comments
-    
-  named_scope :published, lambda {{:conditions => "publishing_date < '#{Time.now.to_formatted_s(:db)}' and draft IS NOT true", 
+
+  named_scope :published, lambda {{:conditions => "publishing_date < '#{Time.now.to_formatted_s(:db)}' and draft IS NOT true",
                                   :order => "publishing_date ASC"}}
-  
-  
+
+
   acts_as_indexed :fields => [:title, :permalink, :excerpt, :body],
                   :index_file => [Rails.root.to_s, "tmp", "index"]
 
   validates_presence_of :title, :permalink, :excerpt, :body
   validates_uniqueness_of :title, :permalink
-  
+
   validates_format_of :permalink, :with => /^(([-_]|[a-z]|\d){1,100})$/, :message => " is invalid, only lowercase alphanumeric character and _-"
   validates_length_of :permalink, :within => 4..99
-  
-  def is_published?
-    if publishing_date >= Time.now || draft != true
-      true
-    else
-      false
-    end
+
+  def published?
+    # A blog post should be published? if:
+    # the publishing date is before now and it is not a draft blog post.
+    publishing_date <= Time.now && !draft
   end
 end
--- app/models/blog_mailer.rb
+++ app/models/blog_mailer.rb
@@ -5,30 +5,18 @@ class BlogMailer < ActionMailer::Base
     recipients  BlogSetting.notification_email
     from        "\"#{RefinerySetting[:site_name]}\" <no-reply@#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}>"
     sent_on     Time.now
-    body        "New comment on #{comment.blog.title} blog post.
-                 Comment author: #{comment.name}
-                 Comment title: #{comment.title}
-                 Comment Body: #{comment.body}"
+    body        :comment => comment
   end
-  
+
   def confirmation(comment, request)
+    site_domain = request.domain(RefinerySetting.find_or_set(:tld_length, 1))
     subject       "#{RefinerySetting[:site_name]} - comment confirmation"
     recipients    comment.email
-    from          "\"#{RefinerySetting[:site_name]}\" <no-reply@#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}>"
+    from          "\"#{RefinerySetting[:site_name]}\" <no-reply@#{site_domain}>"
     sent_on       Time.now
     content_type  "text/html"
-    body          "<html>
-                   <body>
-                   <p>You send a comment '#{comment.blog.title}' blog post at #{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}.
-                   <p>Comment author: #{comment.name}</p>
-                   <p>Comment title: #{comment.title}</p>
-                   <p>Comment Body: #{comment.body}</p>
-                   <hr/>
-                   <p>You're comment is currently not displayed to public, you need to confirm it first by clicking on the following link:</p>
-                   <p><a href='http://#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}/blog/authorize/#{comment.token}'>http://#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}/blog/authorize/#{comment.token}</a></p>
-                   <p><i>if can't click on the link, manually copy & paste this URL into a web browser</i></p>
-                   </body>
-                   </html>"
+    body          :comment => comment,
+                  :site_domain => site_domain
   end
 
 end
