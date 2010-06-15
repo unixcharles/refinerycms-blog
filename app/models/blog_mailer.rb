5efd494a1448cbfbbd516fa80d5acc8825a5c783
Amanda Nicholas <melime@gmail.com>
2010-06-15 11:12:55 +0800

    Tidied blog mailer

--- app/models/blog_mailer.rb
+++ app/models/blog_mailer.rb
@@ -3,20 +3,33 @@ class BlogMailer < ActionMailer::Base
   def notification(comment, request)
     subject     "New comment on #{comment.blog.title}"
     recipients  BlogSetting.notification_email
-    from        "\"#{RefinerySetting[:site_name]}\" <no-reply@#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}>"
+    from        from_address(request)
     sent_on     Time.now
     body        :comment => comment
   end
 
   def confirmation(comment, request)
-    site_domain = request.domain(RefinerySetting.find_or_set(:tld_length, 1))
-    subject       "#{RefinerySetting[:site_name]} - comment confirmation"
+    subject       "#{site_name} - comment confirmation"
     recipients    comment.email
-    from          "\"#{RefinerySetting[:site_name]}\" <no-reply@#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}>"
+    from          from_address(request)
     sent_on       Time.now
     content_type  "text/html"
     body          :comment => comment,
-                  :site_domain => request.domain(RefinerySetting.find_or_set(:tld_length, 1))
+                  :site_domain => site_domain(request)
+  end
+  
+ protected
+ 
+  def from_address(request)
+    "\"#{site_name}\" <no-reply@#{site_domain(request)}>"
+  end
+  
+  def site_domain(request)
+    @domain ||= request.domain(RefinerySetting.find_or_set(:tld_length, 1))
+  end
+ 
+  def site_name
+    RefinerySetting[:site_name]
   end
 
 end
